aa0f1e30571216323508c9f7b2798e86d7c6a7d9
xtrevad <trevor.adelson@gmail.com>
2022-01-03 18:06:15 +1100

    Version 1.0

--- script.js
+++ script.js
@@ -10,20 +10,27 @@ function computerPlay() {
     return result;
 };
 
+
 const computerSelection = computerPlay();
 
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll("div > button");
 
 const para = document.querySelector("p");
 
-const test = Array.from([1, 2, 66]);
+// const test = Array.from([1, 2, 66]);
+
+const container = document.querySelector('#container');
+
+        // *** Current problem: once five rounds are over and a winner is declared, make the buttons break or disappear!
 
 // this calls a round every time a button is clicked, with the button's ID being the player selection.
+
 for (const button of buttons) {
     button.addEventListener('click', () => {
         playRound(button.id, computerSelection);
     });
 };
+
 
 const results = document.querySelector('#results')
 
@@ -48,15 +55,16 @@ results.appendChild(computerScore);
 results.appendChild(feedback);
 
 function playAgain() {
-    // for (const button of buttons) {
-    //     button.removeEventListener('click', () => {
-    //         playRound(button.id, computerSelection);
-    //     });
-    // };
+    for (const button of buttons) {
+        button.setAttribute("disabled", "");
+    };
     let anotherGame = document.createElement('button');
     anotherGame.classList.add('anotherGame');
     anotherGame.textContent = ('Play again?');
     anotherGame.addEventListener('click', () => {
+        for (const button of buttons) {
+            button.removeAttribute("disabled", "");
+        };
         playerPoints = 0;
         computerPoints = 0;
         gameOutput = "Click to play!";
@@ -65,9 +73,13 @@ function playAgain() {
         feedback.textContent = gameOutput;
         gameOutput = "Click to play!";
         results.removeChild(anotherGame);
+        // container.appendChild(document.querySelector('#ROCK'));
+        // container.appendChild(document.querySelector('#PAPER'));
+        // container.appendChild(document.querySelector('#SCISSORS'));
     })
     results.appendChild(anotherGame);
 }
+
 
 function playRound(playerSelection, computerSelection) {
      computerSelection = computerPlay(); 
@@ -84,9 +96,8 @@ function playRound(playerSelection, computerSelection) {
           playerScore.textContent = ("Your score: " + playerPoints);
           if (playerPoints >= 5) {
             feedback.textContent = "You win the game! Congratulations!";
-            computerScore.textContent = ":(";
-            // break the buttons so you can't play again and insert a "play again" button:
-            // make this a function, e.g. 'endOfGame()'
+                // break the buttons so you can't play again and insert a "play again" button:
+                // make this a function, e.g. 'endOfGame()'
             playAgain();
             return;
           }
@@ -99,8 +110,7 @@ function playRound(playerSelection, computerSelection) {
         }
           computerScore.textContent = ("Computer score: " + computerPoints);
           if (computerPoints >= 5) {
-            feedback.textContent = "You lose the game! Loser!"
-            playerScore.textContent = ":(";
+            feedback.textContent = "You lose the game! Loser!";
             playAgain();
             return;
           }
